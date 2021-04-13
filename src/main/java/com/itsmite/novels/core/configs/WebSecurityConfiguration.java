9512035114c62008cb0a0f19c481dc2324874b11
ehabarman <ehabarman@cloud.upwork.com>
2021-04-13 15:55:30 +0300

    Add cors filter

--- src/main/java/com/itsmite/novels/core/configs/WebSecurityConfiguration.java
+++ src/main/java/com/itsmite/novels/core/configs/WebSecurityConfiguration.java
@@ -18,6 +18,11 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 import static com.itsmite.novels.core.constants.EndpointConstants.API_AUTH_V1_ENDPOINT;
 import static com.itsmite.novels.core.constants.EndpointConstants.API_ROLES_V1_ENDPOINT;
@@ -101,5 +106,17 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Bean
     public AccessDeniedHandler accessDeniedHandler() {
         return new AccessDeniedExceptionHandler();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("*"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        configuration.setAllowedOriginPatterns(Arrays.asList("*"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
